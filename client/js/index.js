1ebc4f28169622343b35e4e49c4c695913e207d7
andrew <andrewnijmeh1@gmail.com>
2023-03-19 13:16:06 -0400

    a

--- client/js/index.js
+++ client/js/index.js
@@ -1,4 +1,5 @@
 const $ = document.querySelector.bind(document);
+const stationCode = 'UN';
 
 const { trainDepartures } = { "stationCode": "UN", "allDepartures": { "items": [{ "lineCode": "21", "tripNumber": "21745", "service": "Milton", "transitType": 0, "transitTypeName": "B", "scheduledTime": "20:10", "scheduledDateTime": "2023-03-14T20:10:00", "platform": "", "scheduledPlatform": "", "stopsDisplay": "21H - Square One", "info": null }, { "lineCode": null, "tripNumber": "1033", "service": "Lakeshore West", "transitType": 1, "transitTypeName": "T", "scheduledTime": "20:13", "scheduledDateTime": "2023-03-14T20:13:00", "platform": "5 & 6", "scheduledPlatform": null, "stopsDisplay": "Exhibition-Mimico-Long Branch", "info": "Proceed / Avancez - Train" }, { "lineCode": null, "tripNumber": "7432", "service": "Stouffville", "transitType": 1, "transitTypeName": "T", "scheduledTime": "20:13", "scheduledDateTime": "2023-03-14T20:13:00", "platform": "7 & 8", "scheduledPlatform": null, "stopsDisplay": "Kennedy-Agincourt-Milliken", "info": "Proceed / Avancez - Train" }, { "lineCode": "16", "tripNumber": "16761", "service": "Hamilton / Toronto Express", "transitType": 0, "transitTypeName": "B", "scheduledTime": "20:30", "scheduledDateTime": "2023-03-14T20:30:00", "platform": "", "scheduledPlatform": "", "stopsDisplay": "16  - Hamilton GO", "info": null }, { "lineCode": "21", "tripNumber": "21761", "service": "Milton", "transitType": 0, "transitTypeName": "B", "scheduledTime": "20:30", "scheduledDateTime": "2023-03-14T20:30:00", "platform": "", "scheduledPlatform": "", "stopsDisplay": "21B - Milton GO", "info": null }, { "lineCode": null, "tripNumber": "3533", "service": "Kitchener", "transitType": 1, "transitTypeName": "T", "scheduledTime": "20:34", "scheduledDateTime": "2023-03-14T20:34:00", "platform": "-", "scheduledPlatform": null, "stopsDisplay": "Bloor-Weston-Etobicoke North", "info": "Wait / Attendez - Train" }, { "lineCode": "21", "tripNumber": "21763", "service": "Milton", "transitType": 0, "transitTypeName": "B", "scheduledTime": "20:35", "scheduledDateTime": "2023-03-14T20:35:00", "platform": "", "scheduledPlatform": "", "stopsDisplay": "21D - Lisgar GO", "info": null }, { "lineCode": null, "tripNumber": "9232", "service": "Lakeshore East", "transitType": 1, "transitTypeName": "T", "scheduledTime": "20:36", "scheduledDateTime": "2023-03-14T20:36:00", "platform": "-", "scheduledPlatform": null, "stopsDisplay": "Danforth-Scarborough-Eglinton", "info": "Wait / Attendez - Train" }, { "lineCode": "21", "tripNumber": "21765", "service": "Milton", "transitType": 0, "transitTypeName": "B", "scheduledTime": "20:40", "scheduledDateTime": "2023-03-14T20:40:00", "platform": "", "scheduledPlatform": "", "stopsDisplay": "21H - Square One", "info": null }, { "lineCode": null, "tripNumber": "1733", "service": "Lakeshore West", "transitType": 1, "transitTypeName": "T", "scheduledTime": "20:43", "scheduledDateTime": "2023-03-14T20:43:00", "platform": "-", "scheduledPlatform": null, "stopsDisplay": "Exhibition-Mimico-Long Branch", "info": "Wait / Attendez - Train" }], "page": 1, "pageSize": 10, "totalItemCount": 39 }, "trainDepartures": { "items": [{ "lineCode": null, "tripNumber": "1033", "service": "Lakeshore West", "transitType": 1, "transitTypeName": "T", "scheduledTime": "20:13", "scheduledDateTime": "2023-03-14T20:13:00", "platform": "5 & 6", "scheduledPlatform": null, "stopsDisplay": "Exhibition-Mimico-Long Branch", "info": "Proceed / Avancez - Train" }, { "lineCode": null, "tripNumber": "7432", "service": "Stouffville", "transitType": 1, "transitTypeName": "T", "scheduledTime": "20:13", "scheduledDateTime": "2023-03-14T20:13:00", "platform": "7 & 8", "scheduledPlatform": null, "stopsDisplay": "Kennedy-Agincourt-Milliken", "info": "Proceed / Avancez - Train" }, { "lineCode": null, "tripNumber": "3533", "service": "Kitchener", "transitType": 1, "transitTypeName": "T", "scheduledTime": "20:34", "scheduledDateTime": "2023-03-14T20:34:00", "platform": "-", "scheduledPlatform": null, "stopsDisplay": "Bloor-Weston-Etobicoke North", "info": "Wait / Attendez - Train" }, { "lineCode": null, "tripNumber": "9232", "service": "Lakeshore East", "transitType": 1, "transitTypeName": "T", "scheduledTime": "20:36", "scheduledDateTime": "2023-03-14T20:36:00", "platform": "-", "scheduledPlatform": null, "stopsDisplay": "Danforth-Scarborough-Eglinton", "info": "Wait / Attendez - Train" }, { "lineCode": null, "tripNumber": "1733", "service": "Lakeshore West", "transitType": 1, "transitTypeName": "T", "scheduledTime": "20:43", "scheduledDateTime": "2023-03-14T20:43:00", "platform": "-", "scheduledPlatform": null, "stopsDisplay": "Exhibition-Mimico-Long Branch", "info": "Wait / Attendez - Train" }, { "lineCode": null, "tripNumber": "6333", "service": "Barrie", "transitType": 1, "transitTypeName": "T", "scheduledTime": "20:53", "scheduledDateTime": "2023-03-14T20:53:00", "platform": "-", "scheduledPlatform": null, "stopsDisplay": "DownsviewPark-Rutherford-Maple", "info": "Wait / Attendez - Train" }, { "lineCode": null, "tripNumber": "9034", "service": "Lakeshore East", "transitType": 1, "transitTypeName": "T", "scheduledTime": "21:06", "scheduledDateTime": "2023-03-14T21:06:00", "platform": "-", "scheduledPlatform": null, "stopsDisplay": "Danforth-Scarborough-Eglinton", "info": "Wait / Attendez - Train" }, { "lineCode": null, "tripNumber": "1035", "service": "Lakeshore West", "transitType": 1, "transitTypeName": "T", "scheduledTime": "21:13", "scheduledDateTime": "2023-03-14T21:13:00", "platform": "-", "scheduledPlatform": null, "stopsDisplay": "Exhibition-Mimico-Long Branch", "info": "Wait / Attendez - Train" }, { "lineCode": null, "tripNumber": "7434", "service": "Stouffville", "transitType": 1, "transitTypeName": "T", "scheduledTime": "21:13", "scheduledDateTime": "2023-03-14T21:13:00", "platform": "-", "scheduledPlatform": null, "stopsDisplay": "Kennedy-Agincourt-Milliken", "info": "Wait / Attendez - Train" }, { "lineCode": null, "tripNumber": "3935", "service": "Kitchener", "transitType": 1, "transitTypeName": "T", "scheduledTime": "21:34", "scheduledDateTime": "2023-03-14T21:34:00", "platform": "-", "scheduledPlatform": null, "stopsDisplay": "Bloor-Weston-Etobicoke North", "info": "Wait / Attendez - Train" }], "page": 1, "pageSize": 10, "totalItemCount": 26 }, "busDepartures": { "items": [{ "lineCode": "21", "tripNumber": "21745", "service": "Milton", "transitType": 0, "transitTypeName": "B", "scheduledTime": "20:10", "scheduledDateTime": "2023-03-14T20:10:00", "platform": "", "scheduledPlatform": "", "stopsDisplay": "21H - Square One", "info": null }, { "lineCode": "16", "tripNumber": "16761", "service": "Hamilton / Toronto Express", "transitType": 0, "transitTypeName": "B", "scheduledTime": "20:30", "scheduledDateTime": "2023-03-14T20:30:00", "platform": "", "scheduledPlatform": "", "stopsDisplay": "16  - Hamilton GO", "info": null }, { "lineCode": "21", "tripNumber": "21761", "service": "Milton", "transitType": 0, "transitTypeName": "B", "scheduledTime": "20:30", "scheduledDateTime": "2023-03-14T20:30:00", "platform": "", "scheduledPlatform": "", "stopsDisplay": "21B - Milton GO", "info": null }, { "lineCode": "21", "tripNumber": "21763", "service": "Milton", "transitType": 0, "transitTypeName": "B", "scheduledTime": "20:35", "scheduledDateTime": "2023-03-14T20:35:00", "platform": "", "scheduledPlatform": "", "stopsDisplay": "21D - Lisgar GO", "info": null }, { "lineCode": "21", "tripNumber": "21765", "service": "Milton", "transitType": 0, "transitTypeName": "B", "scheduledTime": "20:40", "scheduledDateTime": "2023-03-14T20:40:00", "platform": "", "scheduledPlatform": "", "stopsDisplay": "21H - Square One", "info": null }, { "lineCode": "61", "tripNumber": "61771", "service": "Richmond Hill", "transitType": 0, "transitTypeName": "B", "scheduledTime": "20:45", "scheduledDateTime": "2023-03-14T20:45:00", "platform": "", "scheduledPlatform": "", "stopsDisplay": "61 - Bloomington GO", "info": null }, { "lineCode": "21", "tripNumber": "21781", "service": "Milton", "transitType": 0, "transitTypeName": "B", "scheduledTime": "21:00", "scheduledDateTime": "2023-03-14T21:00:00", "platform": "", "scheduledPlatform": "", "stopsDisplay": "21B - Milton GO", "info": null }, { "lineCode": "21", "tripNumber": "21783", "service": "Milton", "transitType": 0, "transitTypeName": "B", "scheduledTime": "21:05", "scheduledDateTime": "2023-03-14T21:05:00", "platform": "", "scheduledPlatform": "", "stopsDisplay": "21D - Lisgar GO", "info": null }, { "lineCode": "21", "tripNumber": "21785", "service": "Milton", "transitType": 0, "transitTypeName": "B", "scheduledTime": "21:10", "scheduledDateTime": "2023-03-14T21:10:00", "platform": "", "scheduledPlatform": "", "stopsDisplay": "21H - Square One", "info": null }, { "lineCode": "16", "tripNumber": "16801", "service": "Hamilton / Toronto Express", "transitType": 0, "transitTypeName": "B", "scheduledTime": "21:30", "scheduledDateTime": "2023-03-14T21:30:00", "platform": "", "scheduledPlatform": "", "stopsDisplay": "16  - Hamilton GO", "info": null }], "page": 1, "pageSize": 10, "totalItemCount": 13 } }
 
@@ -67,6 +68,43 @@ const stations = {
   ],
 }
 
+
+const fetchTrainDepartures = async () => {
+  try {
+    const response = await fetch(`https://api.railway.wtf/departures`);
+    const data = await response.json();
+    return data.trainDepartures.items;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// prolly need to update the html its just place holder lol
+const renderTrainDepartures = (trainDepartures) => {
+  const trainDeparturesElement = $('#train-departures');
+  trainDeparturesElement.innerHTML = '';
+
+  trainDepartures.forEach((departure) => {
+    const departureElement = document.createElement('div');
+    departureElement.classList.add('departure');
+    departureElement.innerHTML = `
+        <div class="time">${departure.scheduledTime}</div>
+        <div class="service">${departure.service}</div>
+        <div class="platform">${departure.platform || '-'}</div>
+        <div class="stops">${departure.stopsDisplay}</div>
+        <div class="info">${departure.info || ''}</div>
+      `;
+    trainDeparturesElement.appendChild(departureElement);
+  });
+}
+
+const updateTrainDepartures = async () => {
+  const trainDepartures = await fetchTrainDepartures();
+  renderTrainDepartures(trainDepartures);
+};
+
+setInterval(updateTrainDepartures, 30000);
+
 // Populate select
 let select = $("#stationSelect");
 for (const line in stations) {
@@ -99,3 +137,5 @@ for (const dep of trainDepartures.items) {
   depel.innerHTML = `<span slot="location">${dep.service}</span><span slot="scheduled">${dep.scheduledTime}</span><span slot="platforms">${dep.platform}</span>`
   $("#departures").append(depel);
 }
+
+updateTrainDepartures();
