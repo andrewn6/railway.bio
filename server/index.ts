943a39a558ed6a4bf7833cd8e32b5aa0cabdf39d
andrew <andrewnijmeh1@gmail.com>
2023-03-19 14:08:25 -0400

    add all station codes

--- server/index.ts
+++ server/index.ts
@@ -5,7 +5,69 @@ const app = new Koa();
 const router = new Router();
 
 router.get('/', async (ctx: Context) => {
-  ctx.body = 'gm, welcome to the railway.wtf API';
+  ctx.body = `Welcome to the railway.wtf API, here is all the station codes. You can query them and get departure info via api.railway.wtf/departures?station=UN
+
+  Union Station(UN)
+  Lakeshore East:
+
+  Whitby(WH)
+  Ajax(AJ)
+  Pickering(PI)
+  Rouge Hill(RO)
+  Guildwood(GU)
+  Eglinton(EG)
+  Scarborough(SC)
+  Danforth(DA)
+  Stouffville:
+
+  Old Elm(LI)
+  Stouffville(ST)
+  Mount Joy(MO)
+  Markham(MR)
+  Centennial(CE)
+  Unionville(UI)
+  Milliken(MK)
+  Agincourt(AG)
+  Kennedy(KE)
+  Richmond Hill:
+
+  Bloomington(BL)
+  Gormley(GO)
+  Richmond Hill(RI)
+  Langstaff(LA)
+  Old Cummer(OL)
+  Oriole(OR)
+  Barrie:
+
+  Allendale Waterfront(AD)
+  Barrie South(BA)
+  Bradford(BD)
+  East Gwillimbury(EA)
+  Newmarket(NE)
+  Aurora(AU)
+  King City(KI)
+  Maple(MP)
+  Rutherford(RU)
+  Downsview Park(DO)
+  Lakeshore West:
+
+  Exhibition(EX)
+  Mimico(MI)
+  Long Branch(LO)
+  Port Credit(PO)
+  Clarkson(CL)
+  Oakville(OA)
+  Bronte(BO)
+  Appleby(AP)
+  Burlington(BU)
+  Aldershot(AL)
+  Hamilton(HA)
+  West Harbour(WE)
+  St.Catharines(SCTH)
+  Niagara Falls(NI)
+
+  enjoy :3
+  `;
 });
 
 const getDepartures = async (stationCode: string) => {
