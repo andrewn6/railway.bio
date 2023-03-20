572fdb33f58f8296a8257f3cd91cc5e8208b0d11
pr <pineapplerind.info@gmail.com>
2023-03-20 10:08:05 -0400

    rm: server/dist (build locally)

--- server/dist/routes/departures.d.ts
+++
@@ -1,3 +0,0 @@
-import { Context } from "koa";
-declare const _default: (ctx: Context) => Promise<any>;
-export default _default;
--- server/dist/routes/index.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="koa" />
-declare const _default: {
-    departures: (ctx: import("koa").Context) => Promise<any>;
-    home: (ctx: import("koa").Context) => Promise<string>;
-};
-export default _default;
